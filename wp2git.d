5d1e2e20e5b3ce391e468102ea6cb1b3f5fe99b0
Vladimir Panteleev <vladimir@thecybershadow.net>
2010-06-16 00:56:46 +0300

    Fix downloading

--- wp2git.d
+++ wp2git.d
@@ -17,7 +17,7 @@ int main(string[] args)
 	if (name.length>=2 && name[0]=='"' && name[$-1]=='"')
 		name = name[1..$-1]; // strip quotes
 
-	if (spawnvp(P_WAIT, "curl", ["curl", "-d", "", "http://en.wikipedia.org/w/index.php?title=Special:Export&pages=" ~ name, "-o", "history.xml"]))
+	if (spawnvp(P_WAIT, "curl", ["curl", "-d", "\"\"", "http://en.wikipedia.org/w/index.php?title=Special:Export&pages=" ~ name, "-o", "history.xml"]))
 		throw new Exception("curl error");
 
 	auto xml = new XmlDocument(new File("history.xml"));
