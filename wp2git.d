6f29dd4a2b0dffbda06088c1f2d273e75b381088
Vladimir Panteleev <vladimir@thecybershadow.net>
2013-08-29 21:25:15 +0000

    Update to D2

--- wp2git.d
+++ wp2git.d
@@ -1,4 +1,4 @@
-// Written in the D Programming Language, version 1
+// Written in the D Programming Language (version 2)
 
 import std.stdio;
 import std.process;
@@ -7,7 +7,8 @@ import std.string;
 import std.file;
 import std.conv;
 import std.uri;
-import litexml;
+
+import ae.utils.xmllite;
 
 int main(string[] args)
 {
@@ -37,12 +38,12 @@ int main(string[] args)
 	 
 	if (args.length == 1 || usage)
 	{
-		fwritefln(stderr, "Usage: %s Article_name [OPTION]...", args[0]);
-		fwritefln(stderr, "Create a git repository with the history of the specified Wikipedia article.");
-		fwritefln(stderr, "Supported options:");
-		fwritefln(stderr, " -h  --help		Display this help");
-		fwritefln(stderr, "     --no-import	Don't invoke ``git fast-import'' and only generate the fast-import data");
-		fwritefln(stderr, "     --language LANG	Specify the Wikipedia language subdomain (default: en)");
+		stderr.writefln("Usage: %s Article_name [OPTION]...", args[0]);
+		stderr.writefln("Create a git repository with the history of the specified Wikipedia article.");
+		stderr.writefln("Supported options:");
+		stderr.writefln(" -h  --help		Display this help");
+		stderr.writefln("     --no-import	Don't invoke ``git fast-import'' and only generate the fast-import data");
+		stderr.writefln("     --language LANG	Specify the Wikipedia language subdomain (default: en)");
 		return 2;
 	}
 
@@ -55,10 +56,10 @@ int main(string[] args)
 	if (spawnvp(P_WAIT, "curl", ["curl", "-d", "\"\"", "http://" ~ language ~ ".wikipedia.org/w/index.php?title=Special:Export&pages=" ~ encodeComponent(name), "-o", "history.xml"]))
 		throw new Exception("curl error");
 
-	fwritefln(stderr, "Loading history...");
+	stderr.writefln("Loading history...");
 	string xmldata = cast(string) read("history.xml");
 	std.file.remove("history.xml");
-	auto xml = new XmlDocument(new MemoryStream(xmldata));
+	auto xml = new XmlDocument(xmldata);
 
 	string data = "reset refs/heads/master\n";
 	auto page = xml[0]["page"];
@@ -68,23 +69,23 @@ int main(string[] args)
 		if (child.tag=="revision")
 		{
 			string id = child["id"].text;
-			string summary = child["comment"] ? child["comment"].text : null;
-			string committer = child["contributor"]["username"] ? child["contributor"]["username"].text : child["contributor"]["ip"].text;
+			string summary = child.findChild("comment") ? child["comment"].text : null;
+			string committer = child["contributor"].findChild("username") ? child["contributor"]["username"].text : child["contributor"]["ip"].text;
 			string text = child["text"].text;
-			fwritefln(stderr, "Revision %s by %s: %s", id, committer, summary);
+			stderr.writefln("Revision %s by %s: %s", id, committer, summary);
 			
 			summary ~= "\n\nhttp://" ~ language ~ ".wikipedia.org/w/index.php?oldid=" ~ id;
 			data ~= 
 				"commit refs/heads/master\n" ~ 
 				"committer " ~ committer ~ " <" ~ committer ~ "@" ~ language ~ ".wikipedia.org> " ~ ISO8601toRFC2822(child["timestamp"].text) ~ "\n" ~ 
-				"data " ~ .toString(summary.length) ~ "\n" ~ 
+				"data " ~ to!string(summary.length) ~ "\n" ~ 
 				summary ~ "\n" ~ 
 				"M 644 inline " ~ name ~ ".txt\n" ~ 
-				"data " ~ .toString(text.length) ~ "\n" ~ 
+				"data " ~ to!string(text.length) ~ "\n" ~ 
 				text ~ "\n" ~ 
 				"\n";
 		}
-	write("fast-import-data", data);
+	std.file.write("fast-import-data", data);
 
 	if (noImport)
 		return 0;
@@ -106,5 +107,5 @@ string ISO8601toRFC2822(string s)
 	
 	// 2010-06-15T19:28:44Z
 	// Feb 6 11:22:18 2007 -0500
-	return monthNames[.toInt(s[5..7])-1] ~ " " ~ s[8..10] ~ " " ~ s[11..13] ~ ":" ~ s[14..16] ~ ":" ~ s[17..19] ~ " " ~ s[0..4] ~ " +0000";
+	return monthNames[.to!int(s[5..7])-1] ~ " " ~ s[8..10] ~ " " ~ s[11..13] ~ ":" ~ s[14..16] ~ ":" ~ s[17..19] ~ " " ~ s[0..4] ~ " +0000";
 }
